2f9801934959d167762902cd49e7487cbe0d3e88
CodingQuell <ryequellhunter@gmail.com>
2020-07-15 15:01:13 +0000

    Errors

---
+++ main.py
@@ -0,0 +1,12 @@
+example = "example"
+print("This is an", example, "and has no errors.")
+
+name = "Ellia Deppe"
+fav_artist = "Earl Sweatshirt"
+vacation = "Nosara"
+hobby = "Painting &Skateboarding"
+today = "06/20/22"
+
+print("My name is", name, "and my favorite artist is", fav_artist)
+print("I love to go on vacation to", vacation, "and my two main hobbies are", hobby)
+print("Todays date is:", today)
