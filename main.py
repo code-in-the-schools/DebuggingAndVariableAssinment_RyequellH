3c36f18d053668372a8500f8d88be1ab2eb99c3b
CodingQuell <ryequellhunter@gmail.com>
2020-07-15 16:23:06 +0000

    Diamond

--- main.py
+++ main.py
@@ -10,3 +10,18 @@ today = "06/20/22"
 print("My name is", name, "and my favorite artist is", fav_artist)
 print("I love to go on vacation to", vacation, "and my two main hobbies are", hobby)
 print("Todays date is:", today)
+
+a = "        * "
+b = "     *" "     *"
+c = "  *" "           *"
+d = "*" "               *"
+e = "  *" "           *   "
+f = "    *" "       *"
+g = "        *"
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g) 
